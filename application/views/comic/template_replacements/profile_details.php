776a3799309ddec92fae6cca2916ba12513e5428
sppayne <sppayne@gmail.com>
2018-06-25 22:56:43 +0100

    Added: classes and default styles for profile pages.

--- application/views/comic/template_replacements/profile_details.php
+++ application/views/comic/template_replacements/profile_details.php
@@ -1,6 +1,6 @@
 <?php if(isset($character)){ ?>
 	<?php if($character->filename){ ?>
-		<p><img class="pull-right" title="<?php echo $character->name; ?>" alt="<?php echo $character->name; ?>" src="/assets/characters/<?php echo $character->filename; ?>" /></p>
+		<p class="profile-img-wrap"><img class="profile-img pull-right" title="<?php echo $character->name; ?>" alt="<?php echo $character->name; ?>" src="/assets/characters/<?php echo $character->filename; ?>" /></p>
 	<?php } ?>
 	<?php echo $character->notes; ?>
 	<p><a href="/character_appearances/<?php echo $character->slug; ?>">View all of this character's appearances in the comic</a></p>
