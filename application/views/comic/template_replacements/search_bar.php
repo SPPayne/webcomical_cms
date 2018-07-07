cb4f9c8a542bdd879841e8c854d35e2494909745
sppayne <sppayne@gmail.com>
2018-07-07 13:00:30 +0100

    ID added to search box.

--- application/views/comic/template_replacements/search_bar.php
+++ application/views/comic/template_replacements/search_bar.php
@@ -1,4 +1,4 @@
-<form role="search" method="POST" action="/search">
+<form id="search-box" role="search" method="POST" action="/search">
 	<div class="input-group">
 		<input type="text" name="comic_search" class="form-control" placeholder="Search" <?php if($this->input->post('comic_search')){ ?>value="<?php echo $this->input->post('comic_search'); ?>"<?php } ?> />
 		<div class="input-group-btn">
